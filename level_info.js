acf7eb45ed0205d36383cebe9c0bcaa32ea182bc
ciminilloa <ciminilloa@findlay.edu>
2021-12-20 16:41:21 -0500

    Setup project

---
+++ level_info.js
@@ -0,0 +1,177 @@
+const level_info = [
+
+    {
+        'id': 1,
+        'type': 'Vocab',
+        'english':['I','am/is/are','you','cool','pretty','book','have','car','this','that'],
+        'japanese':['わたし','は~です','あなた','かっこいい','きれい','ほん','が~あります','くるま','これ','それ'],
+        'words': [
+            {
+                'japanese':'わたし',
+                'english':'I',
+                'romanji':'watashi',
+                'example_japanese':'わたし は さら です.',
+                'example_romanji':'watashi wa sara desu.',
+                'example_english':'I am Sara.',
+            },
+            {
+                'japanese':'は~です',
+                'english':'am/is/are',
+                'romanji':'wa~desu',
+                'example_japanese':'わたし は さら です.',
+                'example_romanji':'watashi wa sara desu.',
+                'example_english':'I am Sara.',
+            },
+            {
+                'japanese':'あなた',
+                'english':'you',
+                'romanji':'anata',
+                'example_japanese':'あなた は とむ です.',
+                'example_romanji':'anata wa tomu desu.',
+                'example_english':'You are Tom.',
+            },
+            {
+                'japanese':'かっこいい',
+                'english':'cool',
+                'romanji':'kakkoii',
+                'example_japanese':'あなた は かっこいい です.',
+                'example_romanji':'watashi wa kakkoii desu.',
+                'example_english':'I am cool.',
+            },
+            {
+                'japanese':'きれい',
+                'english':'pretty',
+                'romanji':'kirei',
+                'example_japanese':'あなた は きれい です.',
+                'example_romanji':'anata ha kirei desu.',
+                'example_english':'You are pretty.',
+            },
+            {
+                'japanese':'ほん',
+                'english':'book',
+                'romanji':'hon',
+                'example_japanese':'ほん です.',
+                'example_romanji':'hon desu.',
+                'example_english':'It is a book.',
+            },
+            {
+                'japanese':'が~あります',
+                'english':'have',
+                'romanji':'ga~arimasu',
+                'example_japanese':'わたし は ほん が あります.',
+                'example_romanji':'watashi wa hon ga arimasu.',
+                'example_english':'I have a book.',
+            },
+            {
+                'japanese':'くるま',
+                'english':'car',
+                'romanji':'kuruma',
+                'example_japanese':'あなた は くるま が あります.',
+                'example_romanji':'anata wa kuruma ga arumasu.',
+                'example_english':'You have a car.',
+            },
+            {
+                'japanese':'これ',
+                'english':'this',
+                'romanji':'kore',
+                'example_japanese':'これ は ほん です.',
+                'example_romanji':'kore wa book desu.',
+                'example_english':'This is a book.',
+            },
+            {
+                'japanese':'それ',
+                'english':'that',
+                'romanji':'sore',
+                'example_japanese':'それ は くるま です.',
+                'example_romanji':'sore wa car desu.',
+                'example_english':'That is a car.',
+            }
+        ],
+        'questions': [
+            {
+                'question':'I',
+                'answer':'わたし',
+            },
+            {
+                'question':'is/am/are',
+                'answer':'は~です',
+            },
+            {
+                'question':'You',
+                'answer':'あなた',
+            },
+            {
+                'question':'わたし',
+                'answer':'I',
+            },
+            {
+                'question':'は~です',
+                'answer':'is/am/are',
+            },
+            {
+                'question':'あなた',
+                'answer':'You',
+            },
+            {
+                'question':'Cool',
+                'answer':'かっこいい',
+            },
+            {
+                'question':'Pretty',
+                'answer':'きれい',
+            },
+            {
+                'question':'Book',
+                'answer':'ほん',
+            },
+            {
+                'question':'Have',
+                'answer':'が~あります',
+            },
+            {
+                'question':'Car',
+                'answer':'くるま',
+            },
+            {
+                'question':'This',
+                'answer':'これ',
+            },
+            {
+                'question':'かっこいい',
+                'answer':'Cool',
+            },
+            {
+                'question':'きれい',
+                'answer':'Pretty',
+            },
+            {
+                'question':'ほん',
+                'answer':'Book',
+            },
+            {
+                'question':'が~あります',
+                'answer':'Have',
+            },
+            {
+                'question':'くるま',
+                'answer':'Car',
+            },
+            {
+                'question':'これ',
+                'answer':'This',
+            },
+            {
+                'question':'それ',
+                'answer':'That',
+            },
+            {
+                'question':'That',
+                'answer':'それ',
+            },
+            
+        ]
+    }
+
+
+
+]
